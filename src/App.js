a30e95d29289c23a348ebe75b97b4ef02e707f58
Piyush Shinde <workwithpiyush77@gmail.com>
2025-06-09 10:02:00 +0530

    Completed country search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+function CountryCards({name, image}){
+  return(
+    <div className='countryCard'>
+      <img className='image' src={image} alt={name} />
+      <p className='names'>{name}</p>
+    </div>
+  );
+}
+
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  // if countries.common includes e.target.value set filteredList 
+
+  const handleChange = (e) => {
+    const text = e.target.value.toLowerCase();
+    setSearchText(text);
+    const filtered = countries.filter((names) => names.common.toLowerCase().includes(text));
+    setFilteredList(filtered);
+  }
+
+  useEffect(() => {
+    try{
+      fetch(`https://countries-search-data-prod-812920491762.asia-south1.run.app/countries`)
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+    }
+    catch(e){
+      console.error(e);
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(e) => handleChange(e)} className="search-input" type="text" placeholder='Search for countries...' />
+        <div style={{display:"flex", flexWrap:"wrap", gap:"20px"}}>
+      {(searchText.length > 0 ? filteredList : countries).map((item, index) => (
+        <CountryCards key={`country-${index}`} name={item.common} image={item.png} />
+      ))}
+      </div>
     </div>
   );
 }
